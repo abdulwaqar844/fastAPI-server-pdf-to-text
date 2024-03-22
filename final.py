1e25ba07d5f93c25e00f790beb19913fca11f334
abdulwaqar844 <abdulwaqar844@gmail.com>
2024-03-22 16:53:49 +0500

    updated code

--- final.py
+++ final.py
@@ -2,11 +2,11 @@ import os
 from fastapi.responses import RedirectResponse
 import pdf2image
 from fastapi import FastAPI, UploadFile, File
-from pdf2image import convert_from_bytes
 from paddleocr import PaddleOCR
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-
+import numpy as np
+from PIL import Image
 app = FastAPI()
 
 origins = [
@@ -21,23 +21,17 @@ app.add_middleware(
     allow_methods=["*"],  # You can restrict HTTP methods if needed (e.g., ["GET", "POST"])
     allow_headers=["*"],  # You can restrict headers if needed
 )
-def pdf_to_image(pdf):
-    if not os.path.exists('trial'):
-        os.makedirs('output', exist_ok=True)
-    #convert pdf to pages as images and name then in order
-    pages = pdf2image.convert_from_bytes(pdf,poppler_path="poppler/bin")
-    for i in range(len(pages)):
-        pages[i].save('output\page'+str(i)+'.jpg', 'JPEG')
-
-def ocr_images(images, ocr):
-    ocr_results = []
-    folder = "output"
-
-    result = ocr.ocr(f"{folder}/{images}", cls=True)
-    result = result[0]
-    txts = [line[1][0] for line in result]
-
-    return txts
+def pdf_to_text(pdf):
+    pages = pdf2image.convert_from_bytes(pdf, poppler_path="poppler/bin")
+    ocr = PaddleOCR(use_angle_cls=True, lang='en')
+    text_results = []
+    for page_image in pages:
+        img_np = np.array(page_image)
+        result = ocr.ocr(img_np, cls=True)
+        result = result[0]  # Assuming the first result is sufficient
+        text = '\n'.join([line[1][0] for line in result])  # Join text lines into a single string
+        text_results.append(text)
+    return text_results
 
 @app.get("/")
 async def root():
@@ -50,22 +44,13 @@ async def upload_file(pdf_file: UploadFile):
     if not os.path.exists("output"):
         os.makedirs("output", exist_ok=True)
     
-    ocr = PaddleOCR(use_angle_cls=True, lang='en')
-
     # Read PDF file content
     pdf_content = await pdf_file.read()
-
     # Convert PDF to images
-    pdf_to_image(pdf_content)
-
+    text_results = pdf_to_text(pdf_content)
+    print(text_results)
     # Perform OCR on images
-    results = []
-    for img in os.listdir("output"):
-        ocr_results = ocr_images(img, ocr)
-        print(ocr_results)
-        results.append(ocr_results)
-        os.remove(os.path.join("output", img))
-    return {"message": results}
+    return {"message": text_results}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
