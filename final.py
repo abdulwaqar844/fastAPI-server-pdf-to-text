99375c3f2d500f3b9999e37b6e4450040bdc259f
abdulwaqar844 <abdulwaqar844@gmail.com>
2024-03-22 17:06:06 +0500

    fixed port

--- final.py
+++ final.py
@@ -53,4 +53,4 @@ async def upload_file(pdf_file: UploadFile):
     return {"message": text_results}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
